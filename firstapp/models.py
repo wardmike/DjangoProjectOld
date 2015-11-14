3595f4ff014e6e45b515e74994f1fe85a91019d5
Michael <xenophon.of.athens@gmail.com>
2015-11-14 14:27:11 -0700

    adding database classes

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Models):
+	username = models.CharField(max_length=20)
+
+class Translation(models.Models):
+	english_text = models.CharField(max_length=20)
+	spanish_text = models.CharField(max_length=20)
+
+class Correct(models.Models):
+	user = models.ForeignKey(User)
+	word = models.ForeignKey(Translation)
+	number_correct = models.IntegerField(default=0)
+	number_incorrect = models.IntegerField(default=0)
