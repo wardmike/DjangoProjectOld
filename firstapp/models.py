6e0e22f56ca68cb4a87207114761ef09722057f9
Michael <xenophon.of.athens@gmail.com>
2015-11-14 16:14:57 -0700

    removed users

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-	username = models.CharField(max_length=20)
-	def __str__(self):
-		return self.username
-
 class Translation(models.Model):
 	english_text = models.CharField(max_length=20)
 	spanish_text = models.CharField(max_length=20)
-	def __str__(self):
-		return self.english_text
-
-class Correct(models.Model):
-	user = models.ForeignKey(User)
-	word = models.ForeignKey(Translation)
 	number_correct = models.IntegerField(default=0)
 	number_incorrect = models.IntegerField(default=0)
+	def english_translation(self):
+		return self.english_text
+	def spanish_translation(self):
+		return self.spanish_text
+	def display_correct(self):
+		return number_correct
+	def display_incorrect(self):
+		return self.number_incorrect
 	def __str__(self):
-		return self.number_correct
+		return self.english_text
