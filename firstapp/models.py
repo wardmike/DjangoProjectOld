e25b72f0807240bc65e4a7e33711cce6c52ab43d
Michael <xenophon.of.athens@gmail.com>
2015-11-14 15:23:15 -0700

    added __str__ methods to classes

--- firstapp/models.py
+++ firstapp/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 class User(models.Model):
 	username = models.CharField(max_length=20)
+	def __str__(self):
+		return self.username
 
 class Translation(models.Model):
 	english_text = models.CharField(max_length=20)
 	spanish_text = models.CharField(max_length=20)
+	def __str__(self):
+		return (self.english_text, self.spanish_text)
 
 class Correct(models.Model):
 	user = models.ForeignKey(User)
 	word = models.ForeignKey(Translation)
 	number_correct = models.IntegerField(default=0)
 	number_incorrect = models.IntegerField(default=0)
+	def __str__(self):
+		return (self.number_correct, self.number_incorrect)
