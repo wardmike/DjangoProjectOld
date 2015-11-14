d255e846aec842c182e54aaf428fae988681afc1
Michael <xenophon.of.athens@gmail.com>
2015-11-14 14:35:34 -0700

    fixed typo on models.Model

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Models):
+class User(models.Model):
 	username = models.CharField(max_length=20)
 
-class Translation(models.Models):
+class Translation(models.Model):
 	english_text = models.CharField(max_length=20)
 	spanish_text = models.CharField(max_length=20)
 
-class Correct(models.Models):
+class Correct(models.Model):
 	user = models.ForeignKey(User)
 	word = models.ForeignKey(Translation)
 	number_correct = models.IntegerField(default=0)
